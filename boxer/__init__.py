dbc06df81f88ea84597eaddcde58126e9662d7c0
langirl <1161512420@qq.com>
2019-07-03 23:56:21 +0800

    add boxer dir

---
+++ boxer/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# 用途：
+# 创建日期: 19-7-3 下午11:55
+import time
+
+if __name__ == "__main__":
+    start_t = time.time()
+
+    pass
+
+    print
+    "use time: %s" % (time.time() - start_t)
